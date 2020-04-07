54311f54b8caad50a309d48f2c3a8122581ea16c
kongSteve <stephenngetich@gmail.com>
2020-04-07 19:01:37 +0300

    Start of project

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,13 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) =>{
+    
+    let {region,periodType,reportedCases,population,totalHospitalBeds}= data
+    
+return {
+  data,
+  impact:{
+      currentlyinfected
+  },
+  severeImpact
+}};
 
 export default covid19ImpactEstimator;
