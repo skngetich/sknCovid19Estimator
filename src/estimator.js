9579bdde12d754a1e2172ad9daff6c03ce515c9a
kongSteve <stephenngetich@gmail.com>
2020-04-10 06:30:18 +0300

    replace bitwise with Math,trunc

--- src/estimator.js
+++ src/estimator.js
@@ -32,8 +32,8 @@ Output.prototype.convertToDays = function () {
 
 Output.prototype.rateOfInfection = function () {
   const days = this.convertToDays() / 3;
-  // eslint-disable-next-line no-bitwise
-  return 2 ** (days | 0);
+
+  return 2 ** Math.trunc(days);
 };
 
 Output.prototype.currentlyInfected = function () {
@@ -43,25 +43,20 @@ Output.prototype.infectionsByRequestedTime = function () {
   return this.currentlyInfected() * this.rateOfInfection();
 };
 Output.prototype.severeCasesByRequestedTime = function () {
-  // eslint-disable-next-line no-bitwise
-  return (this.infectionsByRequestedTime() * 0.15) | 0;
+  return Math.trunc(this.infectionsByRequestedTime() * 0.15);
 };
 Output.prototype.hospitalBedsByRequestedTime = function () {
-  // eslint-disable-next-line no-bitwise
-  return ((this.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime()) | 0;
+  return Math.trunc((this.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime());
 };
 
 Output.prototype.casesForICUByRequestedTime = function () {
-  // eslint-disable-next-line no-bitwise
-  return (this.infectionsByRequestedTime() * 0.05) | 0;
+  return Math.trunc(this.infectionsByRequestedTime() * 0.05);
 };
 
 Output.prototype.casesForVentilatorsByRequestedTime = function () {
-  // eslint-disable-next-line no-bitwise
-  return (this.infectionsByRequestedTime() * 0.02) | 0;
+  return Math.trunc(this.infectionsByRequestedTime() * 0.02);
 };
 Output.prototype.dollarsInFlight = function () {
-  // eslint-disable-next-line no-bitwise
   return Math.trunc(this.infectionsByRequestedTime()
   * this.avgDailyIncomePopulation
   * this.avgDailyIncomeInUSD
