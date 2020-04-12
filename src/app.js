76853effe822e861fa5500ca375c3062ea4fad99
Stephen K Ng'etich <stephenngetich@gmail.com>
2020-04-12 20:29:22 +0300

    test eval

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
   res.type('html').sendFile(path.join(`${__dirname}/public/index.html`));
 });
 morgan.token('response-time-ms', function getResponse(req, res) {
-  return `${this['response-time'](req, res, 0)}ms`;
+  return `${this['response-time'](req, res, 0)}0ms`;
 });
 app.use(morgan(':method\t:url\t:status\t:response-time-ms', { stream: accessLogStream }));
 
